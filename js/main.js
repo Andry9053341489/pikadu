ceeb04b6ddbaf0cd303dd73e6a35e0875487c7b8
Andry9053341489 <reg9053341489@yandex.ru>
2020-11-16 00:47:50 +0400

    Весь сайт

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Сщздаем переиенную, в которую положим кнопку меню
+// Сщздаем переменную, в которую положим кнопку меню
 let menuToggle = document.querySelector('#menu-toggle');
 // Создаем переменную, в которую положим меню
 let menu = document.querySelector('.sidebar');
@@ -8,4 +8,16 @@ menuToggle.addEventListener('click', function (event) {
 	event.preventDefault();
 	// Вешаем класс на меню, когда кнопка нажата
 	menu.classList.toggle('visible');
+})
+
+// Сщздаем переменную, в которую положим кнопку меню
+let navToggle = document.querySelector('#nav-toggle');
+// Создаем переменную, в которую положим меню
+let nav = document.querySelector('.header-nav');
+// Отслеживаем клик по кнопке меню и запускаем функцию
+navToggle.addEventListener('click', function (event) {
+	// Отменяем стандартное поведение ссылки
+	event.preventDefault();
+	// Вешаем класс на меню, когда кнопка нажата
+	nav.classList.toggle('visible');
 })
